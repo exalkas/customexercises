1c68e81a275ec42c6b224b68cad5aca58533913b
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-21 09:48:02 +0300

    first commit

---
+++ 02-math_operators.js
@@ -0,0 +1,40 @@
+/**
+ * Exercise 01
+ * Write a function toFahrenheit that converts a temperature from Celsius to Fahrenheit.
+ * Example: toFahrenheit(0) should return 32.
+ */
+
+/**
+ * Exercise 02
+ * Write a function digits that takes a natural number as input and 
+ * returns the number of digits of that number.
+ * Example: digits(2674) should return 4.
+ */
+
+/**
+ * Exercise 03
+ * Which value does x have after execution of the following code?
+ */
+ let x = 3;
+ x++;
+ x = x * 2;
+ x--;
+
+ /**
+  * Exercise 04
+  * Write a function mean that takes 2 numbers and returns their mean value.
+  * Example: mean(1, 2) should return 1.5.
+  */
+
+
+ /**
+  * Exercise 05
+  * Write a JavaScript function to check whether its input parameter is a string or not 
+  * and prints the result to the console
+  */
+
+ /**
+  * Exercise 06
+  * Write a JavaScript program to find the area of a triangle.
+  * the formula is base * height / 2
+  */
