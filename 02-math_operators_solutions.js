c5f07fd7bb7f434956bbdc1575724a5e3127483c
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-21 17:09:14 +0300

    operators solutions added

---
+++ 02-math_operators_solutions.js
@@ -0,0 +1,26 @@
+// 01
+function toFahrenheit(celsius) {
+    return 1.8 * celsius + 32;
+}
+
+// 02
+function digits(n) {
+    return String(n).length;
+}
+
+// 03 -> solution = 7
+
+// 04 
+function mean(x, y) {
+    return (x + y) / 2;
+}
+
+// 05
+function checkString(txt) {
+    console.log(typeof txt)
+}
+
+// 06
+function triangleArea(base, height) {
+    return (base * height) / 2
+}
