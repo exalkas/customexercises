cf3779eae323b6ba04463461eb67d15610630b17
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-22 11:42:18 +0300

    string methods added

---
+++ 03-string_methods.js
@@ -0,0 +1,65 @@
+/**
+ * Exercise 01
+ * Name: String length
+ * Write a function named "length" that accepts a string and returns the number of characters in it
+ * example: length('bible') should return 5
+ */
+
+/**
+ * Exercise 02
+ * Write a function toCase that takes a string and returns that string in lowercase and uppercase 
+ * with - as delimiter.
+ * Example: toCase('Mthatha') should return 'mthatha-MTHATHA'.
+ */
+
+/**
+ * Exercise 03
+ * Write a function shortcut that takes two strings and returns the initial letters of theses strings.
+ * Example: shortcut('Amnesty', 'International') should return 'AI'
+ */
+
+/**
+ * Exercise 04
+ * Write a function firstChar, which returns the first character that is not a space 
+ * when a string is passed.
+ * Example: firstChar(' Rosa Parks ') should return 'R'.
+ */
+
+/**
+ * Exercise 05
+ * Write a function indexOfIgnoreCase taking two strings and determining the 
+ * first occurrence of the second string in the first string. 
+ * The function should be case insensitive.
+ * Example: indexOfIgnoreCase('bit','it') and indexOfIgnoreCase('bit','IT') should return 1
+ */
+
+/**
+ * Exercise 06
+ * Write a function firstWord, taking a string and returning the first word in that string. 
+ * The first word are all characters up to the first space.
+ * Example: firstWord('see and stop') should return 'see'
+ */
+
+/**
+ * Exercise 07
+ * Write a function normalize, that replaces '-' with '/' in a date string.
+ * Example: normalize('20-05-2017') should return '20/05/2017'
+ */
+
+/**
+ * Exercise 08
+ * Write a JavaScript function to split a string and convert it into an array of words
+ */
+
+/**
+ * Exercise 09
+ * What would following code return?
+ * console.log(typeof typeof 1);
+ * Explain your answer
+ */
+
+/**
+ * Exercise 10
+ * Write a function named getExtension that accepts a filename string 
+ * and returns the extension of that filename
+ */
