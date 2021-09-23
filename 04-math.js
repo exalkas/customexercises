21677fb677ac3943abc36e3727c234a1389bb35a
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-23 08:31:20 +0300

    04-math exercises added

---
+++ 04-math.js
@@ -0,0 +1,40 @@
+/**
+ * Exercise 01
+ * Write a function hypotenuse that calculates the length of the hypotenuse
+ *  of a right triangle. 
+ * The length of the hypotenuse is calcultaed from this formula: 
+ * a² + b² = c². 
+ * Since 3² + 4² = 5² applies, hypotenuse(3, 4) should return 5.
+ */
+
+/**
+ * Exercise 02
+ * Write a function midrange, that calculates the midrange of 3 numbers. 
+ * The midrange is the mean of the smallest and largest number.
+ * Example: midrange(3, 9, 1) should return (9+1)/2 = 5
+ */
+
+/**
+ * Exercise 03
+ * Write a function area that calculates the area of a circle. 
+ * The function is given the radius of the circle.
+ * Example: area(1) should return π and area(2) should return 4 * π
+ */
+
+/**
+ * Exercise 04
+ * Write a function dice that returns a random number between 1 and 6 like in dices
+ */
+
+ /**
+  * Exercise 05
+  * Write a function round100 that rounds a number to the nearest hundred.
+  * Example: round100(1749) should return 1700 and round100(856.12) should return 900
+  */
+
+ /**
+  * Exercise 06 Attention: Loops and conditional are required
+  * Write a function parseFirstInt that takes a string and returns the first integer present in the string. 
+  * If the string does not contain an integer, you should get NaN.
+  * Example: parseFirstInt('No. 10') should return 10 and parseFirstInt('Babylon') should return NaN
+  */
