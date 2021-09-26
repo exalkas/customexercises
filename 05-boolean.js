399caa20f6c38dbcdcbd060b47bd0ad672b34ab5
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-26 21:13:44 +0300

    05 boolean added

---
+++ 05-boolean.js
@@ -0,0 +1,62 @@
+/**
+ *  01
+ *  Write a function which accepts 2 numbers and checks whether both numbers are 
+ *  in range 40..60 or in the range 70..100 inclusive
+ */
+
+/**
+ * 02
+ * Write a function to compute the difference between a specified number and 19. 
+ * Returns triple their absolute difference if the specified number is greater than 19
+ */
+
+/**
+ * 03
+ * Write a JavaScript program to check two given numbers 
+ * and return true if one of the numbers is 50 or if their sum is 50
+ */
+
+/**
+ * 04
+ * Write a function to check from two given integers, 
+ * and returns 'Yes' if one is positive and another one is negative
+ * or 'No' if both are positive/negative
+ */
+
+/**
+ * 05
+ * Write a function to create a new string adding "Py" in front of a given string. 
+ * If the given string begins with "Py" then return the original string
+ */
+
+/**
+ * 06
+ * Write a function to create a new string from a given string taking the last 3 characters 
+ * and added at both the front and back. 
+ * The string length must be 3 or more
+ */
+
+/**
+ * 07
+ * Write a function that accepts 3 numbers and returns the biggest of them
+ */
+
+/**
+ * 08
+ * Write a function to find a value which is nearest to 100 
+ * from two different given integer values
+ */
+
+/**
+ * 09
+ * Write a function to check whether a specified character exists within 
+ * the 2nd to 4th position in a given string
+ * E.g.
+ * "Console" and "o" should return true
+ * "Console" and "e" should return false
+ */
+
+/**
+ * 10 - could be challenging
+ * Write a function to check whether the last digit of the three given positive integers is same
+ */
