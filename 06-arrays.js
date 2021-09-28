175e6198351e3b13bb374f7b92cd3d1e4a209e78
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-28 09:39:45 +0300

    06 added

---
+++ 06-arrays.js
@@ -0,0 +1,74 @@
+/**
+ * 01 easy
+ * Crate an array named myLuckyNumbers with the following numbers: 1,2,3,4,5,6,7,8,9,10
+ * Create a function that sums all the numbers of the array
+ */
+
+/**
+ * 02 easy
+ * Write a function toArray that takes 2 values and returns these values in an array.
+ * Example: toArray(5, 9) should return the array [5, 9]
+ */
+
+/**
+ * 03 easy
+ * Write a function getFirstElement that takes an array and returns the first element of the array.
+ * Example: getFirstElement([1, 2]) should return 1
+ */
+
+/**
+ * 04 easy
+ * Write a function setFirstElement that takes an array and one variable. 
+ * Set the first element of the array to the variable 
+ * The array should be returned
+ */
+
+/**
+ * 05 easy
+ * Write a function getLastElement that takes an array and 
+ * returns the last element of the array
+ */
+
+/**
+ * 06 easy
+ * Write a function that accepts an array and a variable. The function should
+ * add the variable to the end of the array
+ * The function should return the array
+ */
+
+/**
+ * 07 easy - medium
+ * Write a function add that adds an element to the end of an array. 
+ * However, the element should only be added if it is not already in the array.
+ * Example: add([1, 2], 3) should return [1, 2, 3] and add([1, 2], 2) should return [1, 2]
+ */
+
+/**
+ * 08 medium
+ * Write a function named list that takes an array of words and returns a string 
+ * by concatenating the words in the array, separated by commas and - the last word - by an 'and'.
+ * An empty array should return an empty string.
+ * Example: list(['Huey', 'Dewey', 'Louie']) should return 'Huey, Dewey and Louie'
+ */
+
+/**
+ * 09 easy
+ * Write a function to get the first element of an array. 
+ * Passing a parameter 'n' will return the first 'n' elements of the array
+ * The function should accept the array and the number of elements
+ */
+
+/**
+ * 10 medium
+ * Write a function which accepts a string of numbers as input and inserts dashes (-) 
+ * between each two even numbers. 
+ * For example if you accept 025468 the output should be 0-254-6-8
+ * The function should return the string output
+ */ 
+
+ /**
+  * 11 medium 
+  * Write a function that accepts an array and  strips from it all items that begin from 'a'
+  * E.g. the array ['apple','banana','orange'] should become ['banana','orange']
+  */
+
