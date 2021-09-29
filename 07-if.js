18bb1a660f8ae43dfacc9924576895da0cef067c
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-09-29 12:01:55 +0300

    07 added

---
+++ 07-if.js
@@ -0,0 +1,129 @@
+/**
+ * 01 easy
+ * Write a function that accepts two integers and returns the larger
+ */
+
+/**
+ * 02 Easy
+ * Write a function that will iterate from 0 to 15. 
+ * For each iteration, it will check if the current number is odd or even, 
+ * and display a message to the screen
+ */
+
+/**
+ * 03 Medium
+ * Write a function that accepts an array of marks of students and computes
+ * and prints, the class average mark:
+ * David	80
+ * Vinoth	77
+ * Divya	88
+ * Ishitha	95
+ * Thomas	68
+ * Then, calculate and print the grade for each student
+ * Grades should be calculated based on these values:
+ * Range	Grade
+ * <60	F
+ * <70	D
+ * <80	C
+ * <90	B
+ * <100	A
+ * Use a switch statement to solve this one
+ */
+
+/**
+ * 04 Easy - medium
+ * Write a function to sum the multiples of 3 and 5 under 1000.
+ */
+
+/**
+ * 05 easy-medium
+ * Create a function to find the longest word in a string and 
+ * return the number of characters of that word.
+ * Example: in "What if we try a super-long word such as otorhinolaryngology"
+ * the answer is 19
+ * "Google do a barrel roll" should return 6
+ */
+
+/**
+ * 06 easy
+ * Write a function equals that checks two values for strict equality. 
+ * If the two values are equal, the string 'EQUAL' should be returned. 
+ * If they are unequal, you should get 'UNEQUAL'.
+ * Example: equals(1, 1) should return 'EQUAL' and equals(1, 2) 
+ * should return 'UNEQUAL'.
+ */
+
+/**
+ * 07 medium
+ * Write a function to check if a string  
+ * ends with the given target string 
+ * Examples:
+ * ("Bastian", "n") should return true
+ * ("Open sesame", "same") should return true
+ */
+
+/**
+ * 08 Easy - medium
+ * Create a function that accepts a string and the number of characters to keep 
+ * from the begining of that string. The rest characters should be discarded 
+ * and in their place should be added the '...' ending
+ * Examples:
+ * ("A-tisket a-tasket A green and yellow basket", 8) 
+ * should return the string A-tisket...
+ * ("Peter Piper picked a peck of pickled peppers", 11) 
+ * should return the string Peter Piper...
+ * ("A-", 1) should return the string A...
+ */
+
+/**
+ * 09 Easy
+ * Write a function addWithSurcharge that adds two amounts with surcharge. 
+ * For each amount less than or equal to 10, the surcharge is 1. 
+ * For each amount greater than 10, the surcharge is 2.
+ * Example: addWithSurcharge(5, 15) should return 23
+ */
+
+/**
+ * 10 medium
+ * Write a function to check if given string is uppercase is not
+ */
+
+/**
+ * 11 easy
+ * Write a function to check if a given argument is a number
+ */
+
+/**
+ * 12 easy - medium
+ * Write a function digitsum that calculates the digit sum of an integer. 
+ * The digit sum of an integer is the sum of all its digits.
+ * Example: digitsum(192) should return 12
+ */
+
+/**
+ * 13 easy - medium
+ * Write a function that converts the case of all characters of a string
+ * Example: 
+ * sIoJK should become SiOjk
+ * aaEErt should become AAeeRT
+ */
+
+/**
+ * 14 easy - medium
+ * Write a function that finds the common elements between 2 given arrays.
+ * Results should be return in array format
+ * Example: 
+ * [1, 2, 3], [1, 2, 4] should return [1, 2]
+ * [1, 8, 10], [1, 2, 4] should return [1]
+ * [1, 8, 10], [1, 8, 10] should return [1, 8, 10]
+ */
+
+/**
+ * 15 easy
+ * Write your own 'includes' function for an array
+ * The function should accept an array and another variable
+ * (Implement it without using the .includes method)
+ * Example:
+ * [1, 2, 3, 4, 5] and 5 should return true
+ * [1, 2, 3, 4, 5] and 6 should return false
+ */
