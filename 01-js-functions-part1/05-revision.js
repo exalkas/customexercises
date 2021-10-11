19607c985400eaaa2d65b4e385185bce51dffcee
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-10-11 11:48:04 +0300

    05 added

---
+++ 01-js-functions-part1/05-revision.js
@@ -0,0 +1,52 @@
+/**
+ * 01
+ * Create a function that accepts 2 arrays and returns an array with
+ * the different items between those 2 arrays
+ * example
+ * diffArray(["grass", "dirt", "pink wool", "dead shrub"],  ["grass", "dirt", "dead shrub"]) should return ["pink wool"]
+ *
+ * diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return [4]
+ */
+
+
+
+/**
+ * 02
+ * Write a function that accepts a letter and returns wether this letter
+ * is upperCase or not. if It's uppercase should return 'YES'
+ * else should return 'NO'
+ */
+
+
+/**
+ * 03
+ * Write a function that counts the number of the odd digits in number.
+ *
+ */
+
+
+/**
+ * 04
+ * Telephone conversation price calculator
+ * Write a program that calculates the price in euros of a
+ * telephone conversation.
+ * The Price table is:
+ * numbers starting from: 0180-1 = 0.039 €/minute
+ * numbers starting from: 0180-2 = 0.06 €/call plus 0.42 €/minute
+ * numbers starting from: 0180-7 = first 30 seconds free, then 0.14 €/minute
+ * numbers starting from 0800 = free of charge.
+ * Please note that prices must be format with 2 digits precision
+ */
+
+/**
+ * 05
+ * Write a function that accepts an array with items followed by one or more
+ * arguments. The function should return another array that doesn't include
+ * any of the elements that have the same values as in the arguments that 
+ * come after the array
+ * Examples: 
+ * clearArray([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1]
+ * clearArray([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1]
+ * clearArray([3, 5, 1, 2, 2], 2, 3, 5) should return [1]
+ * clearArray([2, 3, 2, 3], 2, 3) should return []
+ */
