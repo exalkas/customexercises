4e71818cbecc7d1e5da9c5fe80cc97b13b2023a2
Alkis Kastrisios <85493488+alkasdci@users.noreply.github.com>
2021-11-04 11:49:01 +0200

    class 04 added

---
+++ 04-classes/04.js
@@ -0,0 +1,62 @@
+/**
+ * Create an eshop with classes and OOP
+ * 
+ * Create an eshop class
+ * the constructor should accept an array of products
+ * the owner
+ * the shop address
+ * the billing info (address, post code, VAT number, company name)
+ * this class should declare and initialize as empty arrays:
+ * the products array
+ * the clients array
+ * the carts array
+ * 
+ * Then create another class named Product
+ * Each product should contain:
+ * product description
+ * price
+ * images array
+ * product title
+ * product code
+ * 
+ * Then create the class ProductManager
+ * the constructor should accept an array which should contain all the products
+ * This class should contain the following methods:
+ * add product to the class array
+ * delete product from  the class array
+ * list all products from the class array
+ * 
+ * Then create class Client with a constructor that should contain
+ * name
+ * email
+ * address
+ * birthdate. use format month/date/year or find a way to format dates
+ * 
+ * 
+ * Then create the class ClientManager 
+ * the constructor should accept an array which should contain all the clients 
+ * and the following methods (they all should use the array passed in the class constructor):
+ * add client 
+ * remove client
+ * list clients 
+ * list which clients have birthday today
+ * 
+ * Create a Cart class which should contain
+ * client
+ * products list -> as an empty array - not passed to the constructor
+ * 
+ * Then create the class CartManager
+ * the constructor should accept an array which should contain all the carts 
+ * and the following methods (they all should use the array passed in the class constructor):
+ * add a cart to the carts array
+ * add product to cart
+ * remove product from cart
+ * empty a cart
+ * list products for client
+ * calculate the total price for a user's cart
+ * 
+ * More tasks:
+ * in the first class add the orders array
+ * add similar classes for Orders and Order Manager
+ * with similar properties and methods
+ */
